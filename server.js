b1937a73f35ecb16cf4a00f99d0669658a074135
pranav <pranavamaragani503@gmail.com>
2025-04-21 11:39:56 +0530

    learning route handlers

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
-app.get('/home',(req,res) => {
-    console.log("home page is called");
-    res.send("Hello from Home page");
-})
 
-app.get('/about',(req,res) =>{
-    console.log("about page is called");
-    res.send("this is the aout page")
-})
+app.use('/user',
+    [(req,res,next)=>{
+    console.log("user1")
+
+    // res.send("user ONE")
+    next();
+    
+},
+(req,res,next)=>{
+    console.log("USER2")
+    // res.send("user TWO")
+    next();
+},
+
+(req,res,next)=>{
+    console.log("USER3")
+    res.send("user THREE")
+    next();
+}
+])
+
 app.listen(7777,() => { 
     console.log(`server is running on port 7777....`)
 })
